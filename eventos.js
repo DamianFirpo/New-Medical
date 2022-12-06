b0808870a0621f80dd187c3a8823acf56465cd4c
damianfirpo <damianfirpo9@gmail.com>
2022-12-06 01:44:43 -0300

    add js function for required inputs

---
+++ eventos.js
@@ -0,0 +1,95 @@
+const submitButton = document.getElementById("submitButton");
+
+// sendForm.addEventListener("click", ()=> {
+// alert ("Formulario de contacto enviado exitosamente!");    
+// console.log ("Formulario enviado!");
+// });
+
+// const sendForm = document.getElementById("sendForm");
+
+// sendForm.onclick = ()=>{
+//     alert ("Formulario de contacto enviado exitosamente");
+//     console.log ("Formulario enviado exitosamente!");
+// };
+
+function validateForm() {
+  // Obtener el valor del campo "nomBre"
+  var inputField = document.getElementById("nomBre").value;
+
+  // Comprobar si el campo está vacío
+  if (inputField === "") {
+    // Mostrar un mensaje de error
+    alert("Por favor, rellena el campo *NOMBRE antes de enviar el formulario");
+
+    // Evitar que se envíe el formulario
+    return false;
+  } else {
+    // Si el campo no está vacío, permitir que el formulario se envíe normalmente
+    return true;    
+  }
+}
+
+function validateForm2() {
+  // Obtener el valor del campo "tuMail"
+  var inputField = document.getElementById("tuMail").value;
+
+  // Comprobar si el campo está vacío
+  if (inputField === "") {
+    // Mostrar un mensaje de error
+    alert("Por favor, rellena el campo *E-MAIL antes de enviar el formulario");
+
+    // Evitar que se envíe el formulario
+    return false;
+  } else {
+    // Si el campo no está vacío, permitir que el formulario se envíe normalmente
+    return true;
+  }
+}
+
+function submitForm(event) {
+  event.preventDefault();
+  if (validateForm() && validateForm2()) {
+    document.getElementById("submitButton").scrollIntoView();
+    setTimeout(function() {
+    // Si ambas funciones devuelven true, entonces se muestra el mensaje de éxito
+    alert("Formulario enviado exitosamente. Muchas gracias!")
+  }, 500);
+  
+  } else {
+    // Si alguna de ellas devuelve false, se muestra el mensaje de error
+    
+  }
+}
+
+class Contactado {
+    constructor (nombre, email) {
+        this.nombre = nombre;
+        this.email = email;
+    }
+}
+
+const arrayContactados = [];
+
+const formulario = document.getElementById ("formuLario");
+
+
+submitButton.addEventListener("click", (e)=>{
+    e.preventDefault();
+    const nomBre = document.getElementById("nomBre");
+    const tuMail = document.getElementById("tuMail");    
+    console.log("El nombre ingresado es : " + nomBre.value);
+    console.log ("El mail ingresado es : " + tuMail.value);
+    console.log ("El mensaje ingresado es : " + checkMesagge.value);
+    console.log ("Formulario enviado");
+  
+
+    const contactados = new Contactado (nomBre.value , tuMail.value);
+    arrayContactados.push (contactados);
+    
+    console.log(arrayContactados);
+    
+    formulario.reset ();
+    
+})
+
+  
