4c5bf166860385c2760b1e90eb3d037ec9c8c3db
damianfirpo <damianfirpo9@gmail.com>
2022-12-28 15:37:19 -0300

    add creditcard

--- login.js
+++ login.js
@@ -81,48 +81,6 @@ loginButton.addEventListener("click", () => {
 
 
 
-// loginActive.addEventListener('click', () => {
-// if (loginEmail.value === "alguien@example.com") {
-//   window.location.href = "./sections/login.html";
-  // Swal.fire({
-  //   position: 'top-end',
-  //   icon: 'success', 
-  //   title: 'Bienvenido!',   
-  //   showConfirmButton: false,
-  //   timer: 1500    
-  // })  
-// } else {
-//   loginActive.addEventListener ("click" , () => {
-//     Swal.fire ({
-//       icon: 'error',
-//         title: 'Oops...',
-//         text: 'Datos de inicio de sesión incorrectos!',
-//         html: ` <p> Datos de inicio de sesion incorrectos!</p>
-//             <a href= "" class="masterSolution" id="solucionMaestra" class="swal12-footer">Problemas con el inicio de sesión?</a>`        
-//     })  
-//   } )
-//   loginForm.reset();
-// }
-// });
-
-
-// // Añade un evento click al enlace o botón "iniciar sesión"
-// loginButton.addEventListener('click', function(event) {
-//   event.preventDefault(); // Evita que se siga el enlace
-
-//   loginForm.style.display = 'block'; // Muestra el formulario
-// });
-
-// Obtiene el botón "cerrar"
-// const closeButton = loginForm.querySelector('.close-button');
-
-// // Añade un evento click al botón "cerrar"
-// closeButton.addEventListener('click', function() {
-//   loginForm.style.display = 'none'; // Oculta el formulario
-// });
-
-      
-
 
 
 
